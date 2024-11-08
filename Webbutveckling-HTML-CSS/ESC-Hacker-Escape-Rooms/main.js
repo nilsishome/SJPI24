faf35cb9c253c42bb10217589d2bfe4dd84cd32c
nilsishome <nilsnyberg24@gmail.com>
2024-11-08 17:26:54 +0100

    Patch mobile navigation logic on website state load

--- Webbutveckling-HTML-CSS/ESC-Hacker-Escape-Rooms/main.js
+++ Webbutveckling-HTML-CSS/ESC-Hacker-Escape-Rooms/main.js
@@ -1,32 +1,30 @@
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
-const hamDrop = document.querySelector('.hamburger_menu');
 const hamClick = document.querySelector('.hamburger_click');
+const hamDrop = document.querySelector('.hamburger_menu');
 const mobileNav = document.querySelector('.mobile_nav');
 
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  } 
+} 
 
 hamClick.addEventListener('click', () => {
-
-    if(hamDrop.getAttribute('class') === 'hamburger_menu' && 
-        mobileNav.getAttribute('class') === 'mobile_nav' && 
-        body.getAttribute('class') === '' &&
-        container.getAttribute('class') === 'container')
-    {
-        body.setAttribute('class', 'active');
-        hamDrop.setAttribute('class', 'hamburger_menu active');
-        mobileNav.setAttribute('class', 'mobile_nav active');
-        container.setAttribute('class', 'container active');
-        topFunction();
-    }
-    else
+    if (body.getAttribute('class') === 'active' &&
+        container.getAttribute('class') === 'container active' &&
+        hamDrop.getAttribute('class') === 'hamburger_menu active' && 
+        mobileNav.getAttribute('class') === 'mobile_nav active')
     {
         body.setAttribute('class', '');
+        container.setAttribute('class', 'container');
         hamDrop.setAttribute('class', 'hamburger_menu');
         mobileNav.setAttribute('class', 'mobile_nav');
-        container.setAttribute('class', 'container');
+    }
+    else {
+        body.setAttribute('class', 'active');
+        container.setAttribute('class', 'container active');
+        hamDrop.setAttribute('class', 'hamburger_menu active');
+        mobileNav.setAttribute('class', 'mobile_nav active');
+        topFunction();
     }
 });
