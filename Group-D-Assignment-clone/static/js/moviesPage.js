5717e209f96bcd6af376c243efef3a3b60a50806
nilsishome <nilsnyberg24@gmail.com>
2025-01-24 16:07:50 +0100

    Destruct pathing to API/JSON properties

--- Group-D-Assignment-clone/static/js/moviesPage.js
+++ Group-D-Assignment-clone/static/js/moviesPage.js
@@ -2,14 +2,22 @@ import fetch from "node-fetch";
 
 const API_BASE = "https://plankton-app-xhkom.ondigitalocean.app/api";
 
+function destruct(apiObject) {
+  const movie = {
+    id: apiObject.id,
+    ...apiObject.attributes,
+  };
+  return movie;
+}
+
 export async function retrieveMovies() {
   const response = await fetch(API_BASE + "/movies");
   const apiData = await response.json();
-  return apiData.data;
+  return apiData.data.map(destruct);
 }
 
 export async function retrieveMovie(id) {
   const response = await fetch(API_BASE + "/movies/" + id);
   const apiData = await response.json();
-  return apiData.data;
+  return destruct(apiData.data);
 }
