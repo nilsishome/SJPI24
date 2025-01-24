f99db5c3eadbb493a87ce2ccdd1bff471a8fe400
nilsishome <nilsnyberg24@gmail.com>
2025-01-24 12:51:30 +0100

    Retrieve JSON from API

---
+++ Group-D-Assignment-clone/static/js/moviesPage.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const API_BASE = "https://plankton-app-xhkom.ondigitalocean.app/api";
+
+export async function retrieveMovies() {
+  const response = await fetch(API_BASE + "/movies");
+  const apiData = await response.json();
+  return apiData.data;
+}
+
+export async function retrieveMovie(id) {
+  const response = await fetch(API_BASE + "/movies/" + id);
+  const apiData = await response.json();
+  return apiData.data;
+}
