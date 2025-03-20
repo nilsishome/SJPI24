1a3af4b291eebd76b51d8e929bfe54bda5fa89ea
nilsishome <nilsnyberg24@gmail.com>
2025-03-20 11:00:39 +0100

    Add feedback algorithm + belonging tests

---
+++ Wordle/algorithmA.js
@@ -0,0 +1,25 @@
+export default function feedback(guess, answer) {
+  const RESULT = ["correct", "misplaced", "incorrect"];
+  const array = [];
+  const guessChars = guess.split("");
+  const answerChars = answer.split("");
+
+  for (let i = 0; i < guessChars.length; i++) {
+    let resultValue;
+
+    if (guessChars[i] === answerChars[i]) {
+      resultValue = RESULT[0];
+    } else if (answerChars.includes(guessChars[i])) {
+      resultValue = RESULT[1];
+    } else {
+      resultValue = RESULT[2];
+    }
+
+    array.push({
+      letter: guessChars[i],
+      result: resultValue,
+    });
+  }
+
+  return array;
+}
---
+++ Wordle/algorithmA.test.js
@@ -0,0 +1,83 @@
+import { describe, it, expect } from "@jest/globals";
+import feedback from "./algorithmA";
+
+/*
+    Två olika inputs (text-strängar) ska jämföras med varandra.
+    Ena text-strängen ska vara en gissning, och den andra ska vara ett korrekt svar.
+
+    Funktionaliteten ska berätta om:
+        a. text-strängarna är identiska
+        b. text-strängarna inte är identiska
+    
+    Det ska skrivas ut en array med objekt - ett för varje bokstav - i samma
+    ordning som de förekommer i det gissade ordet med följande attribut:
+        1. letter (bokstaven)
+        2. result (ett av följande värden)
+            2a. 'incorrect' (finns inte med i den andra text-strängen)
+            2b. 'misplaced' (finns med i den andra text-stängen men är felplacerad)
+            2c. 'correct' (korrekt placerad i den andra text-strängen)
+*/
+
+describe("feedback()", () => {
+  it("checks if guess and answer are equal", () => {
+    const output = feedback("Rätt", "Rätt");
+    expect(output).toEqual([
+      {
+        letter: "R",
+        result: "correct",
+      },
+      {
+        letter: "ä",
+        result: "correct",
+      },
+      {
+        letter: "t",
+        result: "correct",
+      },
+      {
+        letter: "t",
+        result: "correct",
+      },
+    ]);
+  });
+
+  it("checks if guess and answer are inequal", () => {
+    const output = feedback("Feel", "Rätt");
+    expect(output).toEqual([
+      {
+        letter: "F",
+        result: "incorrect",
+      },
+      {
+        letter: "e",
+        result: "incorrect",
+      },
+      {
+        letter: "e",
+        result: "incorrect",
+      },
+      {
+        letter: "l",
+        result: "incorrect",
+      },
+    ]);
+  });
+
+  it("returns an array with different result values", () => {
+    const output = feedback("ord", "kod");
+    expect(output).toEqual([
+      {
+        letter: "o",
+        result: "misplaced",
+      },
+      {
+        letter: "r",
+        result: "incorrect",
+      },
+      {
+        letter: "d",
+        result: "correct",
+      },
+    ]);
+  });
+});
