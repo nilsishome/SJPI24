b774ae7d2d8cbbfa7a579f4fbf5fcb9130d079af
nilsishome <nilsnyberg24@gmail.com>
2025-04-08 13:40:14 +0200

    Restructure GameInput component

--- Wordle/components/GameInput.tsx
+++ Wordle/components/GameInput.tsx
@@ -1,44 +1,14 @@
 import React, { JSX, useState, useEffect } from "react";
-import { onType, onDelete } from "../components/inputActions";
 
-const GameInput: React.FC = (): JSX.Element => {
-  const letterBoxes: string[] = Array(5).fill("");
-  const [currentLetters, setCurrentLetters] = useState<string[][]>(
-    Array(6).fill(letterBoxes)
-  );
-  const [currentGuessIndex, setCurrentGuessIndex] = useState<number>(0);
-  const [gameOver, setGameOver] = useState<boolean>(false);
+type Props = {
+  letterBoxes: string[];
+  currentLetters: string[][];
+};
 
-  useEffect(() => {
-    const handleKeyUp = (event: KeyboardEvent): void => {
-      const newLetters: string[][] = currentLetters.map((row: string[]) => [
-        ...row,
-      ]);
-      const userInput: string = event.key;
-      const emptyIndex: number = newLetters[currentGuessIndex].indexOf("");
-
-      if (!gameOver) {
-        if (userInput.match(/^[A-Za-z]$/)) {
-          onType(emptyIndex, newLetters, currentGuessIndex, userInput);
-          setCurrentLetters(newLetters);
-        } else if (userInput === "Backspace") {
-          onDelete(emptyIndex, newLetters, currentGuessIndex);
-          setCurrentLetters(newLetters);
-        } else if (userInput === "Enter") {
-          if (currentGuessIndex < currentLetters.length - 1) {
-            if (emptyIndex === -1) setCurrentGuessIndex(currentGuessIndex + 1);
-          } else setGameOver(true);
-        }
-      }
-    };
-
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [currentLetters, currentGuessIndex, gameOver]);
-
+const GameInput: React.FC<Props> = ({
+  letterBoxes,
+  currentLetters,
+}): JSX.Element => {
   return (
     <div>
       {currentLetters.map(
--- Wordle/src/App.tsx
+++ Wordle/src/App.tsx
@@ -1,6 +1,7 @@
-import React, { JSX } from "react";
+import React, { JSX, useState, useEffect } from "react";
 import GameBoard from "../components/GameBoard";
 import GameInput from "../components/GameInput";
+import { onType, onDelete } from "../components/inputActions";
 
 /*
  *  The plan is to create a Wordle app; using React, TypeScript, server-side
@@ -20,14 +21,50 @@ import GameInput from "../components/GameInput";
  */
 
 const App: React.FC = (): JSX.Element => {
-  // Background color of the game page
+  const letterBoxes: string[] = Array(5).fill("");
+  const [currentLetters, setCurrentLetters] = useState<string[][]>(
+    Array(6).fill(letterBoxes)
+  );
+  const [currentGuessIndex, setCurrentGuessIndex] = useState<number>(0);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+
   document.body.style = "background-color: #474747";
+
+  useEffect(() => {
+    const handleKeyUp = (event: KeyboardEvent): void => {
+      const newLetters: string[][] = currentLetters.map((row: string[]) => [
+        ...row,
+      ]);
+      const userInput: string = event.key;
+      const emptyIndex: number = newLetters[currentGuessIndex].indexOf("");
+
+      if (!gameOver) {
+        if (userInput.match(/^[A-Za-z]$/)) {
+          onType(emptyIndex, newLetters, currentGuessIndex, userInput);
+          setCurrentLetters(newLetters);
+        } else if (userInput === "Backspace") {
+          onDelete(emptyIndex, newLetters, currentGuessIndex);
+          setCurrentLetters(newLetters);
+        } else if (userInput === "Enter") {
+          if (currentGuessIndex < currentLetters.length - 1) {
+            if (emptyIndex === -1) setCurrentGuessIndex(currentGuessIndex + 1);
+          } else setGameOver(true);
+        }
+      }
+    };
+
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [currentLetters, currentGuessIndex, gameOver]);
 
   return (
     <main className="App">
       <h1 id="title">Wordle</h1>
       <section id="boardWrapper">
-        <GameInput />
+        <GameInput letterBoxes={letterBoxes} currentLetters={currentLetters} />
       </section>
       <section id="keyWrapper">
         <GameBoard />
