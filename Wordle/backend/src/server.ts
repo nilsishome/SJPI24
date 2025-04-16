47d8ab1754724fa40f6d0ff89fbc9b651612af91
nilsishome <nilsnyberg24@gmail.com>
2025-04-16 10:58:54 +0200

    Add rimraf to delete unnecessary files in dist folder

--- Wordle/backend/src/server.ts
+++ Wordle/backend/src/server.ts
@@ -3,8 +3,16 @@ import express from "express";
 const app = express();
 const PORT = 5080;
 
-app.get("/api/data", (req, res) => {});
+app.get("/api/data", (req, res) => {
+  res.status(200).json({
+    data: {
+      name: "Jalmar",
+      animal: "dog",
+      cute: "YES",
+    },
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is open on port: ${PORT}`);
+  console.log(`Server is open on http://localhost:${PORT}`);
 });
