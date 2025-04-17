c9d9eb3ca9474250ffa53edb3d60a658466bdaff
nilsishome <nilsnyberg24@gmail.com>
2025-04-17 11:29:00 +0200

    Add utils file with an asynchronous function choosing a random word from local words list

---
+++ Wordle/backend/src/utils.ts
@@ -0,0 +1,18 @@
+import wordSelection from "./logic/algorithmB.js";
+
+export const randomWord = async (length: number, allowRepetition: boolean) => {
+  const URL = "../words.json";
+  const response = await import(URL, { with: { type: "json" } });
+
+  if (!response) {
+    console.error("ERROR");
+  } else {
+    const payload = await response;
+    const data = JSON.parse(JSON.stringify(payload));
+    const words = data.default.words;
+
+    const randomWord = wordSelection(words, length, allowRepetition);
+
+    return randomWord;
+  }
+};
