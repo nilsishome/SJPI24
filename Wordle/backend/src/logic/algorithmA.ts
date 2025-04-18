99dc517bccb469e9ef2e27382742d96fbba23339
nilsishome <nilsnyberg24@gmail.com>
2025-04-18 15:26:57 +0200

    Fix misplaced bug

--- Wordle/backend/src/logic/algorithmA.ts
+++ Wordle/backend/src/logic/algorithmA.ts
@@ -4,14 +4,13 @@ export default function feedback(guess: string, answer: string) {
   const guessChars: string[] = guess.split("");
   const answerChars: string[] = answer.split("");
 
+  // Checks if a letter is correct or incorrect
   for (let i = 0; i < guessChars.length; i++) {
     let resultValue: string;
 
     if (guessChars[i] === answerChars[i]) {
       resultValue = RESULT[0];
       answerChars.splice(i, 1, "");
-    } else if (answerChars.includes(guessChars[i])) {
-      resultValue = RESULT[1];
     } else {
       resultValue = RESULT[2];
     }
@@ -20,6 +19,18 @@ export default function feedback(guess: string, answer: string) {
       letter: guessChars[i],
       result: resultValue,
     });
+  }
+
+  // Checks if a letter is misplaced
+  for (let i = 0; i < guessChars.length; i++) {
+    let resultValue: string;
+    const letter = array[i].letter;
+
+    if (array[i].result === RESULT[2] && answerChars.includes(letter)) {
+      resultValue = RESULT[1];
+      array[i].result = resultValue;
+      answerChars.splice(answerChars.indexOf(letter), 1, "");
+    }
   }
 
   return array;
