bc16bf4729908a66a8951dd32e0177e623a4f713
nilsishome <nilsnyberg24@gmail.com>
2025-04-30 09:27:36 +0200

    Make name property required in Highscore Schema

--- Wordle/backend/src/Highscore.ts
+++ Wordle/backend/src/Highscore.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const highscoreSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    require: true,
+  },
   time: Number,
   guesses: [String],
   wordLength: Number,
